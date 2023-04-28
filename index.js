d2c44a53eb0de37f9f51c9706708c1c310109b10
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-04-28 17:35:36 +0100

    add

---
+++ index.js
@@ -0,0 +1,12 @@
+// document.getElementById("count-el").innerText = 5 
+
+let count = 5
+
+console.log(count) 
+
+let myAge = 23 
+
+ let humanDogRatio = 7 
+
+let myDogAge = myAge * humanDogRatio 
+console.log(myDogAge) 
