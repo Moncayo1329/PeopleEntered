6226e8c88407a3a1ccad1eeb0a975e12db859b75
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-04-28 22:30:07 +0100

    project

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // document.getElementById("count-el").innerText = 5 
 
-let count = 5
+let countEl = document.getElementById("count-el")
 
-console.log(count) 
+console.log(countEl) 
 
-let myAge = 23 
+let count = 0  
 
- let humanDogRatio = 7 
+function increment () { 
+    count = count + 1 
+    countEl.innerText = count
+    console.log(count) 
+}
 
-let myDogAge = myAge * humanDogRatio 
-console.log(myDogAge) 
+function save () {  
+    console.log(count) 
+    
+     
+}
+
+let message =" You have three new notifications"
+
+console.log(message) 
+
+let nAme = "Michael" 
+let greeting = " Hi my name is " 
+
+let myGreeting = greeting + " , " + nAme 
+
+console.log(myGreeting) 
